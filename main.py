027ac979b721428e982c438d794c5cace40b7e2d
Sambath Seddhini (Aroza) <seddhini.ken@gmail.com>
2026-07-20 02:11:24 +1000

    updated character json and scene engine, finished content up to owlbear fight

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ def run_game():
     current_id = "intro_01"
     scene = {}
     with open("data/scenes/intro.json", 'r', encoding='utf-8') as f:
-        scenes = json.load(f)
-        scene = {scene["id"]: scene for scene in scenes}
+        scenes = json.load(f) # turns a list of dicts
+        scene = {scene["id"]: scene for scene in scenes} # turn it into a dict with id as the key
 
     while current_id is not None:
-        print(scene[current_id]["text"])
+        text = scene[current_id]["text"] # get the current line of text
+        print(text)
         input("\n")
-        current_id = scene[current_id]["next"]
+        current_id = scene[current_id]["next"] # get the next line of text
     print("End of intro.")
 
 if __name__ == "__main__":
