6b1df6d5ae32811192560000f15ced5e15914969
Sambath Seddhini (Aroza) <seddhini.ken@gmail.com>
2026-07-26 23:39:03 +1000

    Map respective sprites to fursttry.json and add postfight scene

--- scenes.py
+++ scenes.py
@@ -18,19 +18,23 @@ def win_scene():
 
     with open("data/scenes/owlbearfight_win.json", 'r', encoding='utf-8') as f:
         scenes = json.load(f) # returns a list of dicts
-        char_line = {line["speaker"]: line for line in scenes}
+        char_line = {line["speaker"]: line for line in scenes} # sort lines by the speaker
 
         ignore_chars = ["fursttryl", "mother_owlbear", "top_baby_owlbear", "bottom_baby_owlbear"]
+
+        # play the party member's dialogue if they are alive
         for character in state.owlbearfight_alive:
             if (state.owlbearfight_alive[character] == True and character not in ignore_chars):
                 load_and_play("data/scenes/owlbearfight_win.json", char_line[character]["id"])
 
+        # count how many baby owlbears are alive
         count_babies = 0
         if (state.owlbearfight_alive["top_baby_owlbear"] == True and state.owlbearfight_alive["bottom_baby_owlbear"] == True):
             count_babies = 2
         elif (state.owlbearfight_alive["top_baby_owlbear"] == True or state.owlbearfight_alive["bottom_baby_owlbear"] == True):
             count_babies = 1
-        
+
+        # possibly play additional dialogue depending on count_babies
         if (count_babies == 1):
             load_and_play("data/scenes/owlbearfight_win.json", "line_10")
         elif (count_babies == 2):
@@ -40,5 +44,34 @@ def win_scene():
     postfight_scene()
 
 def postfight_scene():
-    # add postfight scene here
-    return
+    load_and_play("data/scenes/postfight.json", "line_01")
+
+    with open("data/scenes/postfight.json", 'r', encoding='utf-8') as f:
+            scenes = json.load(f) # returns a list of dicts
+            char_line = {line["speaker"]: line for line in scenes} # sort lines by the speaker
+
+            deaths = 0
+            ignore_chars = ["mother_owlbear", "top_baby_owlbear", "bottom_baby_owlbear"]
+
+            # count the number of dead party members
+            for character in state.owlbearfight_alive:
+                if (state.owlbearfight_alive[character] == False and character not in ignore_chars):
+                    deaths += 1
+
+            if (deaths > 0):
+                load_and_play("data/scenes/postfight.json", "line_06")
+
+            load_and_play("data/scenes/postfight.json", "line_08")
+
+            # play the party member's dialogue if they are dead
+            for character in state.owlbearfight_alive:
+                if (state.owlbearfight_alive[character] == False and character not in ignore_chars):
+                    load_and_play("data/scenes/postfight.json", char_line[character]["id"])
+
+            load_and_play("data/scenes/postfight.json", "line_14")
+
+            if (deaths > 0):
+                load_and_play("data/scenes/postfight.json", "line_17")
+
+            load_and_play("data/scenes/postfight.json", "line_23")
+    
